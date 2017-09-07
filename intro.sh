0026827063ce6a6d925a86c082c5cc0cc38da2d3
Jessica Cole <jessica.cole@wsu.edu>
2017-09-06 22:20:41 -0700

    Modifying intro.sh with personal details.

--- intro.sh
+++ intro.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Hello, let me introduce myself"
+echo "Hello, let me introduce myself. I'm a man of wealth and taste. Pleased to meet you."
 
-NAME=Michael
-HOMETOWN=Indianapolis
+NAME=Jesse
+HOMETOWN="Las Vegas"
 MAJOR=CS
-FAVORITE_HOBBY=read
+FAVORITE_HOBBY="... uhh, what is a hobby again?"
 
 echo "My name is $NAME. I am originally from $HOMETOWN. My major is $MAJOR and in my free time I like to $FAVORITE_HOBBY"
